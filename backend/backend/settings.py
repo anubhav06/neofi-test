7d8a723b289c566c328e504a8531431ffecff406
Anubhav Gupta <mail.anubhav06@gmail.com>
2024-02-18 22:43:30 +0530

    add domain
    Signed-off-by: Anubhav Gupta <mail.anubhav06@gmail.com>

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,11 +31,13 @@ ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = [
     'http://127.0.0.1:8000',
     'http://localhost:3000'
+    'https://neofi-test-production.up.railway.app'
 ]
 
 CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:8000',
     'http://localhost:3000'
+    'https://neofi-test-production.up.railway.app'
 ]
 
 # Application definition
